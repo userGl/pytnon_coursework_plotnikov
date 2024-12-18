75132004d95c86dc1331917e167c645892e572c1
Gleb Plotnikov <g_plotnikov@mail.ru>
2024-12-18 15:43:11 +0300

    18.12 день

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pytesseract
 from PIL import Image
 
 #C:\Users\Gleb\AppData\Local\Programs\Tesseract-OCR
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Gleb\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\plotnikov.gleb\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
-image = Image.open("test.jpg")
+image = Image.open("test1.png")
 string = pytesseract.image_to_string(image, lang='rus')
 print(string)
