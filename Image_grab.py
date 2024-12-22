8f253e1436c6b1a22ff0dfcd222044e99ea54b8e
user_1000 <g_plotnikov@mai.ru>
2024-12-22 18:51:27 +0300

    22.12.2024 вечер

--- Image_grab.py
+++
@@ -1,9 +0,0 @@
-# pip install pillow
-from PIL import ImageGrab
-
-img = ImageGrab.grabclipboard()
-# or ImageGrab.grab() to grab the whole screen!
-img.show()
-print(img)
-img.save('test1.png')
-# <PIL.BmpImagePlugin.DibImageFile image mode=RGB size=380x173 at 0x16A43064DA0>
--- main.py
+++
@@ -1,9 +0,0 @@
-import pytesseract
-from PIL import Image
-
-#C:\Users\Gleb\AppData\Local\Programs\Tesseract-OCR
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\plotnikov.gleb\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
-
-image = Image.open("test1.png")
-string = pytesseract.image_to_string(image, lang='rus')
-print(string)
