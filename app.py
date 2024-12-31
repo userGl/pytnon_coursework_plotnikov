60c60c0e70209ae2bc0034debaabbc4d62ccc0f8
user_1000 <g_plotnikov@mai.ru>
2024-12-31 23:04:41 +0300

    Добавлена jinja

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, File, UploadFile
 import tesseract
 
+import httpx
+
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+
+
+
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 # @app.get("/")
 # def read_root():
@@ -20,6 +29,22 @@ async def upload_file(file: UploadFile = File(...)):
        f.write(content)
     text = tesseract.ocr_recognize(file_path, 'rus+eng')
     return {"filename": file.filename, "size": len(content), "text": text }
+
+# Статическая HTML-страница
+@app.get("/static-page", response_class=HTMLResponse)
+async def static_page(request: Request):
+    text="Привет мир"    
+    return templates.TemplateResponse("static_page.html", {"request": request, "text": text}, )
+
+# Отображение данных в таблице
+@app.get("/demo-data", response_class=HTMLResponse)
+async def demo_data(request: Request):
+    data = [
+        {"name": "John", "age": 30},
+        {"name": "Jane", "age": 25},
+        {"name": "Alice", "age": 35}
+    ]
+    return templates.TemplateResponse("demo_data.html", {"request": request, "data": data})
 
 
 #uvicorn app:app --reload
