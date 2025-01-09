35fbe8e00dbf6c3761e4321086bd8c360a3a4be5
user_1000 <g_plotnikov@mai.ru>
2025-01-09 22:52:08 +0300

    08.01.2025

--- tesseract.py
+++ tesseract.py
@@ -4,10 +4,10 @@ import os
 
 #pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Gleb\Documents\01.Обучение\01.Python\Python_coursework\pytnon_coursework_plotnikov\Tesseract-OCR\tesseract.exe'
 #pytesseract.pytesseract.tesseract_cmd = r'..\..\..\tesseract-ocr\tesseract.exe'
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Gleb\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+#pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Gleb\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 #pytesseract.pytesseract.tesseract_cmd = r'C:\Users\plotnikov.gleb\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
-#tesseract_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'Tesseract-OCR', 'tesseract.exe'))
-#pytesseract.pytesseract.tesseract_cmd = tesseract_path
+tesseract_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'Tesseract-OCR', 'tesseract.exe'))
+pytesseract.pytesseract.tesseract_cmd = tesseract_path
 
 
 def ocr_recognize(image="test.jpg", lang='rus'):
